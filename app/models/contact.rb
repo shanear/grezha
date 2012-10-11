674c53eb15827c823ba9b974f51548f7792856c7
Shane Russell <shane1337@gmail.com>
2012-10-11 11:53:47 -0500

    Change S3 bucket name

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,6 +7,6 @@ class Contact < ActiveRecord::Base
     s3_credentials: {
       access_key_id: ENV["S3_ACCESS_KEY_ID"],
       secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
-      bucket: "daughters_contacts"
+      bucket: "daughters"
     }
 end
