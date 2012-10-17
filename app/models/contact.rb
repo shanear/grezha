bb5f034f9e97ee90876e2145e346af7e8367e578
Shane Russell <shane1337@gmail.com>
2012-10-17 10:17:18 -0500

    S3 storage only for production

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,11 +2,5 @@ class Contact < ActiveRecord::Base
   attr_accessible :bio, :birthday, :name, :picture
 
   has_attached_file :picture,
-    styles: { medium: "200", thumb: "50x50#" },
-    storage: :s3,
-    s3_credentials: {
-      access_key_id: ENV["S3_ACCESS_KEY_ID"],
-      secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
-      bucket: "daughters"
-    }
+    styles: { medium: "200", thumb: "50x50#" }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,16 @@ Daughters::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # Paperclip storage
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      access_key_id: ENV["S3_ACCESS_KEY_ID"],
+      secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
+      bucket: "daughters"
+    }
+  }
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
