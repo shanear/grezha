9e4e6565193dadcaae1f36598d8526233da9024b
Shane Russell <shane1337@gmail.com>
2014-02-20 10:20:13 -0800

    More Rails 4 upgrade stuff

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,5 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Daughters::Application.config.secret_token = '287cb34d07bbca6b59aa6867123255af3b31de8b6eb0350c5350c342ff124b85d829f3c3e015de3bbb5d9dd4b57a8b3128832f37601f723251b915e55f2ab0a7'
+
+Daughters::Application.config.secret_key_base = '1e022c011e0e34e90725c020ab0f8839158c1a4b7a126a37d49c452726522ab88fdeb6476998e606872786aace694c6f530599d797a5769bda29b0bbfb3e1c0c'
