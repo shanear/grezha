423f43a7b9b3eb933316b826542d69a910185e8e
Shane Russell <shane1337@gmail.com>
2014-04-01 10:28:58 -0700

    Fix vehicle spec

--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe Vehicle do
   it "should validate uniqueness of license plate" do
     @vehicle = FactoryGirl.create(:vehicle)
-    @duplicate_vehicle = FactoryGirl.build(:vehicle,
-      license_plate: @vehicle.license_plate)
+    @duplicate_vehicle = FactoryGirl.build(:vehicle)
 
+    @duplicate_vehicle.license_plate = @vehicle.license_plate
     @duplicate_vehicle.should_not be_valid
   end
 end
