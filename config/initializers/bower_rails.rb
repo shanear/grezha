48e6d1393b32a1123f369af5a868b856f65c3831
Peiying Wen <wenpeiying@gmail.com>
2014-05-11 16:23:19 -0700

    don't configure bower-rails in prod

--- config/initializers/bower_rails.rb
+++ config/initializers/bower_rails.rb
@@ -1,5 +1,7 @@
-BowerRails.configure do |bower_rails|
-  # bower_rails.install_before_precompile = true
-  bower_rails.resolve_before_precompile = true
-  bower_rails.clean_before_precompile = true
+if (Rails.env.development? || Rails.env.test?)
+  BowerRails.configure do |bower_rails|
+    # bower_rails.install_before_precompile = true
+    bower_rails.resolve_before_precompile = true
+    bower_rails.clean_before_precompile = true
+  end
 end
