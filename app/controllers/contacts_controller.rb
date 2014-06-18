bb499978faf0fa237cb1ec213cd76c39295bce5d
Shane Russell <shane1337@gmail.com>
2014-06-17 18:44:52 -0700

    Filter contacts by org in birthdays as well

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,9 @@ class ContactsController < ApplicationController
   before_filter :require_logged_in
 
   def birthdays
-    @recent = Contact.with_recent_birthday
-    @upcoming = Contact.with_upcoming_birthday
+    contacts = Contact.where(organization_id: current_user.organization_id)
+
+    @recent = contacts.with_recent_birthday
+    @upcoming = contacts.with_upcoming_birthday
   end
 end
