1261a1c447060800b7a747e78d5045ca706589b5
Shane Russell <shane1337@gmail.com>
2014-11-12 09:39:25 -0800

    Fix relationship spec

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -2,12 +2,13 @@ require 'spec_helper'
 
 describe Relationship do
 	let(:relationship) { FactoryGirl.build(:relationship) }
+
   context "builds remote id" do
-  	 it "automatically populates it with a valid id" do
-        relationship = FactoryGirl.build(:relationship, remote_id: nil)
-        relationship.save!
-        expect(relationship.remote_id).to match(/[A-Za-z0-9]{8}/)
-      end
+    it "automatically populates it with a valid id" do
+      relationship = FactoryGirl.build(:relationship, remote_id: nil)
+      relationship.save!
+      expect(relationship.remote_id).to match(/[A-Za-z0-9]{8}/)
+    end
   end
 
   context "validity of relationships" do
@@ -22,15 +23,5 @@ describe Relationship do
   		relationship.contact = nil
   		expect(relationship).to_not be_valid
   	end
-
-  	it "is invalid when no relationship type" do
-  		relationship = FactoryGirl.build(:relationship)
-  		relationship.relationship_type = nil
-  		expect(relationship).to_not be_valid
-  	end
-
   end
-
-
-
 end
