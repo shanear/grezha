1daebc69f071e8d50f3c02f640a1e9e2c012a81f
Shane Russell <shane1337@gmail.com>
2015-02-14 10:02:16 -0800

    Upgrade to EmberCLI 1.15 :raising_hand:

--- ember/Brocfile.js
+++ ember/Brocfile.js
@@ -2,7 +2,9 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  vendorFiles: {'handlebars.js': null}
+});
 
 // Add offline manifest in production
 if (app.env === 'production') {
