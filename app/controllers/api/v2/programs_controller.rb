7f9047798590a852ece6884a505df7e7c9b898a1
Shane Russell <shane1337@gmail.com>
2015-05-25 10:01:29 -0700

    Create program endpoint

--- app/controllers/api/v2/programs_controller.rb
+++ app/controllers/api/v2/programs_controller.rb
@@ -6,9 +6,25 @@ class Api::V2::ProgramsController < Api::BaseController
     respond_with programs
   end
 
+  def create
+    @program = programs.new(create_program_params)
+    @program.organization_id = current_user.organization_id
+
+    if @program.save()
+      render json: @program
+    else
+      render json: { errors: @program.errors }, status: 422
+    end
+  end
+
   private
 
   def programs
     Program.where(organization_id: current_user.organization_id)
+  end
+
+  def create_program_params
+    params[:program][:remote_id] = params[:program][:id]
+    params.required(:program).permit(:remote_id, :name)
   end
 end
--- spec/controllers/api/v2/programs_controller_spec.rb
+++ spec/controllers/api/v2/programs_controller_spec.rb
@@ -12,28 +12,41 @@ describe Api::V2::ProgramsController do
   describe "while logged in" do
     before { authorize_api(user) }
 
-    let(:program) {
-      FactoryGirl.create(:program,
-        name: "Moving Club",
-        organization_id: organization.id
-      )
-    }
-
-    let(:other_program) {
-      FactoryGirl.create(:program,
-        name: "Anti-Moving Club",
-        organization_id: organization.id + 1
-      )
-    }
-
     describe "GET #index" do
-      before { [program, other_program] }
+      let(:program) {
+        FactoryGirl.create(:program,
+          name: "Moving Club",
+          organization_id: organization.id
+        )
+      }
+
+      let(:other_program) {
+        FactoryGirl.create(:program,
+          name: "Anti-Moving Club",
+          organization_id: organization.id + 1
+        )
+      }
 
       it "gets programs for the user's organization" do
+        [program, other_program]
         get :index, format: :json
 
         expect(json["programs"].length).to be(1)
         expect(json["programs"].first["name"]).to eq("Moving Club")
+      end
+    end
+
+    describe "POST #create" do
+      it "creates a new event in user's organization with a remote id" do
+        post :create, program: {
+          id: "ABCDEFG1",
+          name: "Book club"
+        }
+
+        new_program = Program.last
+        expect(new_program.remote_id).to eq("ABCDEFG1")
+        expect(new_program.organization_id).to eq(organization.id)
+        expect(new_program.name).to eq("Book club")
       end
     end
   end
